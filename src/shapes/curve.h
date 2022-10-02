0bf8c6300ec6526cb93ef235fece04ba5faf4565
bumzacktrara <bumzacktrara@gmail.com>
2022-10-02 20:35:25 +0200

    fmt and make methods extern

--- src/shapes/curve.h
+++ src/shapes/curve.h
@@ -42,55 +42,65 @@
 #include "shape.h"
 
 namespace pbrt {
-struct CurveCommon;
+
+    extern Point3f BlossomBezier(const Point3f p[4], Float u0, Float u1, Float u2);
+
+    extern struct CurveCommon;
 
 // CurveType Declarations
-enum class CurveType { Flat, Cylinder, Ribbon };
+    enum class CurveType {
+        Flat, Cylinder, Ribbon
+    };
 
 // CurveCommon Declarations
-struct CurveCommon {
-    CurveCommon(const Point3f c[4], Float w0, Float w1, CurveType type,
-                const Normal3f *norm);
-    const CurveType type;
-    Point3f cpObj[4];
-    Float width[2];
-    Normal3f n[2];
-    Float normalAngle, invSinNormalAngle;
-};
+    extern struct CurveCommon {
+        CurveCommon(const Point3f c[4], Float w0, Float w1, CurveType type,
+                    const Normal3f *norm);
+
+        const CurveType type;
+        Point3f cpObj[4];
+        Float width[2];
+        Normal3f n[2];
+        Float normalAngle, invSinNormalAngle;
+    };
 
 // Curve Declarations
-class Curve : public Shape {
-  public:
-    // Curve Public Methods
-    Curve(const Transform *ObjectToWorld, const Transform *WorldToObject,
-          bool reverseOrientation, const std::shared_ptr<CurveCommon> &common,
-          Float uMin, Float uMax)
-        : Shape(ObjectToWorld, WorldToObject, reverseOrientation),
-          common(common),
-          uMin(uMin),
-          uMax(uMax) {}
-    Bounds3f ObjectBound() const;
-    bool Intersect(const Ray &ray, Float *tHit, SurfaceInteraction *isect,
-                   bool testAlphaTexture) const;
-    Float Area() const;
-    Interaction Sample(const Point2f &u, Float *pdf) const;
+    extern class Curve : public Shape {
+    public:
+        // Curve Public Methods
+        Curve(const Transform *ObjectToWorld, const Transform *WorldToObject,
+              bool reverseOrientation, const std::shared_ptr<CurveCommon> &common,
+              Float uMin, Float uMax)
+                : Shape(ObjectToWorld, WorldToObject, reverseOrientation),
+                  common(common),
+                  uMin(uMin),
+                  uMax(uMax) {}
 
-  private:
-    // Curve Private Methods
-    bool recursiveIntersect(const Ray &r, Float *tHit,
-                            SurfaceInteraction *isect, const Point3f cp[4],
-                            const Transform &rayToObject, Float u0, Float u1,
-                            int depth) const;
+        Bounds3f ObjectBound() const;
 
-    // Curve Private Data
-    const std::shared_ptr<CurveCommon> common;
-    const Float uMin, uMax;
-};
+        bool Intersect(const Ray &ray, Float *tHit, SurfaceInteraction *isect,
+                       bool testAlphaTexture) const;
 
-std::vector<std::shared_ptr<Shape>> CreateCurveShape(const Transform *o2w,
-                                                     const Transform *w2o,
-                                                     bool reverseOrientation,
-                                                     const ParamSet &params);
+        Float Area() const;
+
+        Interaction Sample(const Point2f &u, Float *pdf) const;
+
+    private:
+        // Curve Private Methods
+        bool recursiveIntersect(const Ray &r, Float *tHit,
+                                SurfaceInteraction *isect, const Point3f cp[4],
+                                const Transform &rayToObject, Float u0, Float u1,
+                                int depth) const;
+
+        // Curve Private Data
+        const std::shared_ptr<CurveCommon> common;
+        const Float uMin, uMax;
+    };
+
+    std::vector<std::shared_ptr<Shape>> CreateCurveShape(const Transform *o2w,
+                                                         const Transform *w2o,
+                                                         bool reverseOrientation,
+                                                         const ParamSet &params);
 
 }  // namespace pbrt
 
